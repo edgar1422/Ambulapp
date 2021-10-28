310c095d5513d078c26d331eea58c36123bce174
Edgar Suaza <edgar1422@gmail.com>
2021-10-28 12:01:07 -0500

    add

--- app/src/main/java/com/example/prueba_repo/MainActivity.java
+++ app/src/main/java/com/example/prueba_repo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.prueba_repo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "0nDestroy", Toast.LENGTH_SHORT).show();
+
+
     }
 }
