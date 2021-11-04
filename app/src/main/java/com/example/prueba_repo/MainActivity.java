627b9f8d622d750c02ea1ddaf6b8a05999ddc7f0
Viviana Urrego <viliurr@gmail.com>
2021-11-04 10:34:30 -0500

    Se agregaron los archivos xml

--- app/src/main/java/com/example/prueba_repo/MainActivity.java
+++ app/src/main/java/com/example/prueba_repo/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ingreso);
 
         nota1_txt = (EditText) findViewById(R.id.nota1);
         nota2_txt = (EditText) findViewById(R.id.nota2);
