82b0fc001b75a2e41bccdbb4393d344de8a8faf5
Andrea Velasquez <patico_091@hotmail.com>
2021-11-07 09:57:17 -0500

    Se agrega la pantalla de Bienvenida de la aplicación.

--- app/src/main/java/com/example/prueba_repo/MainActivity.java
+++ app/src/main/java/com/example/prueba_repo/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso);
 
-        correo_txt = (EditText) findViewById(R.id.et_correo);
-        contrasena_txt = (EditText) findViewById(R.id.et_contraseña);
+        //correo_txt = (EditText) findViewById(R.id.et_correo);
+        //contrasena_txt = (EditText) findViewById(R.id.et_contraseña);
 
 
 
