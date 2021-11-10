a5d939a863934bc3da5d8a120a845613a183936c
RubenMoncayo <90462951+rubenmoncayo@users.noreply.github.com>
2021-11-10 17:58:02 -0500

    Se agregaron tablayouts(pestañas) a los menus de la interfaz de Registro_usu (favor revisar, no funciona bien).

---
+++ app/src/androidTest/java/com/example/prueba_repo/PagerController.java
@@ -0,0 +1,4 @@
+package com.example.prueba_repo;
+
+public class PagerController {
+}
--- app/src/main/java/com/example/prueba_repo/Registro_usu.java
+++ app/src/main/java/com/example/prueba_repo/Registro_usu.java
@@ -1,14 +1,60 @@
 package com.example.prueba_repo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.example.prueba_repo.Controlador.PagerController;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class Registro_usu extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1, tab2;
+
+    PagerController pagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usu);
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+        tab1 = findViewById(R.id.tabusuario);
+        tab2 = findViewById(R.id.tabambulancia);
+
+        pagerAdapter = new PagerController(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if(tab.getPosition()==1) {
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
     }
 }
