22ade6f39259d36e4c201d910f799431b2b14159
Viviana Urrego <viliurr@gmail.com>
2021-11-12 06:26:28 -0500

    quedo funcionando todo los item del menu esto se hizo a través del uso de framents

--- app/src/main/java/com/example/prueba_repo/ActivityPrincipal.java
+++ app/src/main/java/com/example/prueba_repo/ActivityPrincipal.java
@@ -1,41 +1,91 @@
 package com.example.prueba_repo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class ActivityPrincipal extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal_amb);
 
 
+    public class ActivityPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        DrawerLayout mDrawerLayout;
+        NavigationView navigationView;
+        Toolbar toolbar;
 
-        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_principal_amb);
 
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
+            mDrawerLayout = findViewById(R.id.drawerLayout);
+            navigationView = findViewById(R.id.nav_view);
+            toolbar = findViewById(R.id.toolbar);
+
+            getSupportFragmentManager().beginTransaction().add(R.id.content, new Home_Fragment()).commit();
+            setTitle("Inicio");
+            // setup toolbar
+            setSupportActionBar(toolbar);
+            navigationView.setNavigationItemSelectedListener(this);
+
+
+            final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+            findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+
+            });
+
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setItemIconTintList(null);
+
+        }
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            selectItemNav(item);
+            return true;
+        }
+
+        private void selectItemNav(MenuItem item) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            switch (item.getItemId()) {
+
+                case R.id.nav_Inicio:
+                    ft.replace(R.id.content, new Home_Fragment()).commit();
+                    break;
+                case R.id.nav_profile:
+                    ft.replace(R.id.content, new Profile_Fragment()).commit();
+                    break;
+                case R.id.nav_notification:
+                    ft.replace(R.id.content, new Notificaciones_Fragment()).commit();
+                    break;
+                case R.id.nav_history:
+                    ft.replace(R.id.content, new Historial_Fragment()).commit();
+                    break;
+                case R.id.nav_configuration:
+                    ft.replace(R.id.content, new Configuraciones_Fragment()).commit();
+                    break;
+                case R.id.nav_help:
+                    ft.replace(R.id.content, new Ayuda_Fragment()).commit();
+                    break;
             }
-
-        });
-
-
-        NavigationView navigationView = findViewById(R.id.navigationview);
-        navigationView.setItemIconTintList(null);
-
-
+            setTitle(item.getTitle());
+            mDrawerLayout.closeDrawers();
+        }
     }
-
-
-
-}
