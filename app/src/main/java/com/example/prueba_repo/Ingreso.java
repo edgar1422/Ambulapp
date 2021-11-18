e76e9c178ac8f7b2b3baa6d5fda1c5f71c55f874
Viviana Urrego <viliurr@gmail.com>
2021-11-18 13:12:58 -0500

    cree base de datos

--- app/src/main/java/com/example/prueba_repo/Ingreso.java
+++ app/src/main/java/com/example/prueba_repo/Ingreso.java
@@ -6,15 +6,22 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -26,6 +33,10 @@ public class Ingreso extends AppCompatActivity {
 
     private EditText correo_txt, contrasena_txt;
     private Button btn_login, btn_register;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "tester";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +48,28 @@ public class Ingreso extends AppCompatActivity {
         btn_login = (Button) findViewById(R.id.button2);
         btn_register = (Button) findViewById(R.id.button3);
         check_Permission();
+        mAuth = FirebaseAuth.getInstance();
+
+
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "Ingreso al boton");
 
                 String correo_str = correo_txt.getText().toString();
                 String contrasena_str = contrasena_txt.getText().toString();
 
-                if(correo_str.equals("prueba") && contrasena_str.equals("prueba") ) {
+                signIn(correo_str,contrasena_str);
+               /* if(correo_str.equals("prueba") && contrasena_str.equals("prueba") ) {
+
+
                     Intent i = new Intent(Ingreso.this, ActivityPrincipal.class);
-                    startActivity(i);
+                   startActivity(i);
                 } else {
-                    Toast.makeText(Ingreso.this, "Datos incorrectos, vuelva a intentarlo", Toast.LENGTH_LONG).show();
-                }
+                    //Toast.makeText(Ingreso.this, "Datos incorrectos, vuelva a intentarlo", Toast.LENGTH_LONG).show();
+                }*/
 
             }
         });
@@ -65,6 +84,52 @@ public class Ingreso extends AppCompatActivity {
         });
 
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        Log.d(TAG, "Ingreso al login");
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+
+// If sign in fails, display a message to the user.
+                            Log.d(TAG, "signInWithEmail:success");
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Ingreso.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Log.d("tester","test");
+        Toast.makeText(Ingreso.this, "Actualizado ",
+                Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Ingreso.this, ActivityPrincipal.class);
+        startActivity(i);
+    }
+
+
+
+
+
     private void check_Permission() {
 
         Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
