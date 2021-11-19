135c2d230e88a295fbea00aa24f89acf10f61cc3
Viviana Urrego <viliurr@gmail.com>
2021-11-19 17:17:53 -0500

    complete datos ambulancia

--- app/src/main/java/com/example/prueba_repo/DatosAmbulancia.java
+++ app/src/main/java/com/example/prueba_repo/DatosAmbulancia.java
@@ -2,34 +2,105 @@ package com.example.prueba_repo;
 
 public class DatosAmbulancia {
 
-    private String nombre;
-    private String Apellido;
+    private String nombres,email,apellidos,eps,contasena,direccion,ciudad;
+    private int telefono,fecha_nacimiento;
 
-    public DatosAmbulancia(String nombre, String apellido) {
-        this.nombre = nombre;
-        Apellido = apellido;
+    public DatosAmbulancia(String nombres, String apellidos, String email, String contasena, String direccion, String ciudad, int telefono, int fecha_nacimiento) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.eps = eps;
+        this.contasena = contasena;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.telefono = telefono;
+        this.fecha_nacimiento = fecha_nacimiento;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getNombres() {
+        return nombres;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
     }
 
-    public String getApellido() {
-        return Apellido;
+    public String getApellidos() {
+        return apellidos;
     }
 
-    public void setApellido(String apellido) {
-        Apellido = apellido;
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+
+    public String getContasena() {
+        return contasena;
+    }
+
+    public void setContasena(String contasena) {
+        this.contasena = contasena;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(int fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
     @Override
     public String toString() {
         return "datosAmbulancia" +
-                "Nombre='" + nombre + '\'' +
-                ", Apellido='" + Apellido+ '\'' +
+                "Nombres ='" + nombres + '\'' +
+                ", Apellidos ='" + apellidos+ '\'' +
+                ", Email='" + email+ '\'' +
+                ", Contaseña ='" + contasena+ '\'' +
+                ", Telefono ='" + telefono+ '\'' +
+                ", Eps='" + eps+ '\'' +
+                ", Fecha de nacimiento ='" + fecha_nacimiento+ '\'' +
+                ", Dirección ='" + direccion+ '\'' +
+                ", Ciudad ='" + ciudad+ '\'' +
                 '}';
     }
 }
